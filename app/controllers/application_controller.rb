31e5cfffa07c90c62009329c1ad44e0768b2e686
mr-koww <mr-koww@yandex.ru>
2019-09-11 14:14:54 +0300

    connect with backend (axios, jbuilder)

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::Base
-  def index
+  def index; end
+
+  def list
+    @list = [
+      {
+        id: 1,
+        title: 'My first task'
+      },
+      {
+        id: 2,
+        title: 'My second task'
+      }
+    ]
   end
 end
--- app/javascript/app.vue
+++ app/javascript/app.vue
@@ -1,28 +1,37 @@
 <template>
-  <div>
-    <input v-model="title">
-    <button @click="addTodo">Add todo</button>
-    <todo :title="todo.title" v-for="todo in list" @deleteTodo="deleteTodo"></todo>
+  <div v-if="loading">
+    <p>Загрузка</p>
+  </div>
+  <div v-else>
+    <div v-if="error">
+      <p>Ошибка</p>
+    </div>
+    <div v-else>
+      <input v-model="title">
+      <button @click="addTodo">Add todo</button>
+      <todo :title="todo.title" v-for="todo in list" @deleteTodo="deleteTodo"></todo>
+    </div>
   </div>
 </template>
 
 <script>
+  import axios from 'axios'
   import Todo from 'app/components/todo'
 
   export default {
-    data () {
+    data: function () {
       return {
+        loading: true,
+        error: false,
         title: '',
-        list: [
-          {
-            title: 'My first todo'
-          }
-        ]
+        list: []
       }
     },
-    computed: {
-    },
-    watch: {
+    created: function() {
+      axios.get('/list')
+        .then((response) => this.list = response.data)
+        .catch(() => this.error = true)
+        .finally(() => this.loading = false)
     },
     methods: {
       addTodo() {
